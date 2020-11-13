4d3abd7cc2d0ff21138e3f5636c16cfb110c0a36
Jeremy <than2314@gmail.com>
2020-11-13 14:27:05 +0700

    Update db

--- insert.php
+++ insert.php
@@ -28,7 +28,7 @@ mysqli_close($conn);
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'itflabdb.mysql.database.azure.com', 'thetimes@itflabdb', 'PooMlmp99', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
